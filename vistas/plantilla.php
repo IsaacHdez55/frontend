98975039eb3437379c24ba8049c1c7cdd499e184
Isaac Hernández <isaachernndez55@gmail.com>
2018-11-07 20:22:04 -0500

    Initial commit

---
+++ vistas/plantilla.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width-device-width, initial-scale-1.0, minium-scale-1.0, maximun-scale-1.0, user-scalable-no">
+
+	<meta name="title" content="Tienda Virtual">
+
+	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto minima aliquid quidem quisquam, ducimus. Accusamus, officiis
+">
+
+	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing, elit, Iusto, minima, aliquid">
+
+	<title>Tienda Virtual</title>
+
+	<link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+
+	<link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+
+	<script src="js/plugins/jquery.min.js"></script>
+
+	<script src="js/plugins/bootstrap.min.js"></script>
+
+</head>
+<body>
+	
+<?php
+/*=============================================
+CABEZOTE
+=============================================*/
+
+include "modulos/cabezote.php";
+
+?>
+
+</body>
+</html>
